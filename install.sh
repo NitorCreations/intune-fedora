7ef7feb1eb85ae425f1bda6d6e9c446ee0489948
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2023-09-22 10:49:07 +0200

    The timer target needs to be started to run perdiodically in the background

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ sudo dnf install webkit2gtk4.0
 sudo rpm -Uvh --nodeps intune-portal-*.rpm
 sudo dnf install msalsdk-dbusclient*.rpm microsoft-identity-broker-*.rpm
 
+systemctl --user enable /lib/systemd/user/intune-agent.timer
+
 echo "You need to restart your machine for changes to apply"
