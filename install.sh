c08771c79eb93f7f618f0f32f58429526fa1e99d
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2023-09-22 11:26:49 +0200

    Add script for installing edge

--- install.sh
+++ install.sh
@@ -6,4 +6,16 @@ sudo dnf install msalsdk-dbusclient*.rpm microsoft-identity-broker-*.rpm
 
 systemctl --user enable /lib/systemd/user/intune-agent.timer
 
+if [ ! -f /etc/yum.repos.d/microsoft-edge.repo ]; then
+  cat << EOF | sudo tee /etc/yum.repos.d/microsoft-edge.repo
+[microsoft-edge]
+name=microsoft-edge
+baseurl=https://packages.microsoft.com/yumrepos/edge/
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+fi
+sudo dnf install microsoft-edge-beta
+
 echo "You need to restart your machine for changes to apply"
