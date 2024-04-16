69b18b4a71917b4784aaae3a0895c42b4a3498b1
Pasi Niemi <pasi.niemi@nitor.com>
2024-04-16 14:35:43 +0300

    Fix for docker

--- build.sh
+++ build.sh
@@ -5,34 +5,42 @@ set -e -o pipefail
 
 rm -f *.rpm
 
-if which podman > /dev/null; then
+if which podman 2>&1 > /dev/null; then
   PODMAN=podman
-elif which docker > /dev/null; then
+elif which docker 2>&1 > /dev/null; then
   PODMAN=docker
 else
   echo "No container runtime found"
   exit 1
 fi
 
-$PODMAN build . | tee build.log
+$PODMAN build . 2>&1 | tee build.log
 
-ID=$(tail -n 1 build.log)
+if [ $PODMAN = podman ]; then
+  ID=$(tail -n 1 build.log)
+else
+  ID=$(grep "writing image" build.log | awk -NF " |:" '{ print $5 }')
+fi
 
 rm -rf tmp && mkdir tmp
 cd tmp
 
 echo "Extracting rpms from image..."
-$PODMAN image save $ID | tar xf - --wildcards '*.tar'
-for i in */layer.tar; do
-  tar xvf $i --wildcards '*.rpm' 2> /dev/null || true
-done
+if [ $PODMAN = podman ]; then
+  $PODMAN image save $ID | tar xf - --wildcards '*.tar'
+  for i in */layer.tar; do
+    tar xvf $i --wildcards '*.rpm' 2> /dev/null || true
+  done
+else
+  $PODMAN image save $ID | tar xf -
+  for i in $(find blobs -type f); do
+    tar -xvf $i --wildcards '*.rpm' 2> /dev/null || true
+  done
+fi
 
 mv *.rpm ..
 cd ..
 rm -rf tmp
-
-## anyone can figure out how to fix curl dependency inside inttune-portal.rpm?
-# nothing provides libcurl.so.4(CURL_OPENSSL_4)(64bit) needed by intune-portal
 
 echo
 echo "Install packages by running:"
