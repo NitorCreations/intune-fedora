6ac513c13e8e82578a5eed7a2d3d6c404d2dab51
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2024-06-25 11:53:03 +0300

    local cannot be used outside of a function

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-local token="$(vault -l packages.nitor.zone.apikey)"
+token="$(vault -l packages.nitor.zone.apikey)"
 for i in *.rpm; do
   echo "Uploading $i"
   curl --progress-meter -u __token__:"$token" --upload-file $i https://packages.nitor.zone/repository/yum/intune/$i
